e95d3145a9390b54832020078e1b3f3b2ef8b915
d.ivaka <ivakus@mail.ru>
2018-03-28 12:57:29 +0300

    Версия приложения 8, реализовал работу с датой родов.

--- app/src/main/java/ru/c0ner/babyplaner/Fragments/Setting.java
+++ app/src/main/java/ru/c0ner/babyplaner/Fragments/Setting.java
@@ -1,21 +1,26 @@
 package ru.c0ner.babyplaner.Fragments;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+
 
 import ru.c0ner.babyplaner.R;
 
 
 
-public class Setting extends babyFragment implements Button.OnClickListener{
+public class Setting extends babyFragment implements View.OnClickListener{
 
     final String STORADGE_NAME = "baby_v1";
     final String USER_NAME = "USER_NAME";
@@ -25,7 +30,7 @@ public class Setting extends babyFragment implements Button.OnClickListener{
     public babyStoradge mStor;
     String mUser_name = "Гость";
     int mDay = 180;
-
+    Time time;
     EditText mUsername;
     TextView mDay_do_rodov;
     public final static String TAG = "SettingTAG";
@@ -39,6 +44,9 @@ public class Setting extends babyFragment implements Button.OnClickListener{
         mDay_do_rodov = (TextView) v.getRootView().findViewById(R.id.setting_day_do_rodov);
         Button btn = (Button) v.getRootView().findViewById(R.id.settings_save);
         btn.setOnClickListener(this);
+        mDay_do_rodov.setOnClickListener(this);
+        time = new Time(Time.getCurrentTimezone());
+        time.setToNow();
         // mStor = new babyStoradge (getContext());
         ReadData();
         return v;
@@ -52,7 +60,8 @@ public class Setting extends babyFragment implements Button.OnClickListener{
             mDay = mStor.getDataInt(DAYS_RODOV);
         }
         mUsername.setText(mUser_name);
-        mDay_do_rodov.setText(""+mDay);
+        // mDay_do_rodov.setText(""+mDay);
+        mDay_do_rodov.setText(" " + time.weekDay + "." + time.month + "." + time.year);
     }
     public void SaveDada (){
 
@@ -69,10 +78,27 @@ public class Setting extends babyFragment implements Button.OnClickListener{
 
 
                 mUser_name = mUsername.getText().toString();
-                mDay = Integer.parseInt(mDay_do_rodov.getText().toString());
+              //  mDay = Integer.parseInt(mDay_do_rodov.getText().toString());
                 SaveDada();
                 mItemSelectListiner.ItemSelect(TAG, "Setting", 0);
 
         }
+        if (v.getId() == R.id.setting_day_do_rodov) {
+
+            DatePickerDialog tpd = new DatePickerDialog(getContext(), myCallBack, time.year, time.month,time.monthDay);
+            tpd.show();
+
+        }
     }
+
+    DatePickerDialog.OnDateSetListener myCallBack = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            int myYear = year;
+            int myMonth = monthOfYear;
+            int myDay = dayOfMonth;
+            mDay_do_rodov.setText(" " + myDay + "/" + myMonth + "/" + myYear);
+        }
+    };
 }
